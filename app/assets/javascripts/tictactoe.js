7344abb7522d902d591f886af5f114a0edf175c0
Heather Christman <efl7a@yahoo.de>
2018-04-27 10:40:24 -0400

    some of the changes are good, but 10+ tests are failing due to some functionality changes.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,16 +1,13 @@
 $(document).ready(function() {
   attachListeners()
   setTurn()
-  if(checkWinner() || turn >= 8){
+  if(checkWinner() || turn >= 8) {
     $("td").off("click")
   }
 })
 
-var turn
-var board = function () {
-  clearGame()
-  return createBoardArray()
-}
+var turn = turn || 0
+var board = board || ['', '', '', '', '', '', '', '', '']
 var id = id || 0
 
 function player() {
@@ -31,7 +28,7 @@ function setMessage(message) {
 }
 
 function checkWinner() {
-  var wins = [
+  const wins = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -44,7 +41,7 @@ function checkWinner() {
   board = createBoardArray()
   result =  wins.some(function (combo) {
     if(board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]] && board[combo[0]] != ""){
-        setMessage(`Player ${board[combo[0]]} Won!`)
+      setMessage(`Player ${board[combo[0]]} Won!`)
       return true;
     }
   })
@@ -52,19 +49,16 @@ function checkWinner() {
 }
 
 function doTurn(square) {
-    var approvedMove = updateState(square)
-    var result = checkWinner()
+    let approvedMove = updateState(square)
+    let result = checkWinner()
     if (turn >= 8){
       setMessage("Tie game.")
     }
     if(result || turn >= 8){
       // set a timer
-      saveGame()
-      clearGame()
-      board = createBoardArray()
-      turn = 0
-      id = 0
-      $("#message").text("")
+      $("td").off("click")
+      // I think the setTimeout is nice so that you can see your when.  However, it makes a 3 test fail.
+      setTimeout(newGame, 1000)
     } else {
       if(approvedMove) {
         turn ++
@@ -84,8 +78,7 @@ function attachListeners() {
 
 function saveGame(){
   if(id === 0) {
-    var state = $.param({"state": board})
-    $.post("/games", state).done(function(game){
+    $.post("/games", {state: board}).done(function(game){
       id = game["data"]["id"]
     })
     // $.ajax({
@@ -111,9 +104,9 @@ function saveGame(){
 
 function previousGame(){
   $.get('/games').done(function(data) {
-    var i = $("#games > button").length
+    let i = $("#games > button").length
     for(i; i < data["data"].length; i++) {
-      var id = data["data"][i]["id"]
+      id = data["data"][i]["id"]
       $("#games").append('<button class="js-game" id="'+ id +'">' + id + '</button>')
     }
     $(".js-game").on("click", loadGame)
@@ -121,15 +114,18 @@ function previousGame(){
 }
 
 function clearGame() {
-  var squares = $("td")
-  for(var i = 0; i < squares.length; i++ ){
+  let squares = $("td")
+  for(let i = 0; i < squares.length; i++ ){
     squares[i].innerHTML = ""
     }
+  createBoardArray()
   id = 0
   turn = 0
+  $("#message").text("")
 }
 
 function loadGame(){
+  clearGame()
    id = $(this).attr("id")
   $.get("/games/"+ id).done(function(data){
     board = data["data"]["attributes"]["state"]
@@ -137,16 +133,21 @@ function loadGame(){
     setTurn()
     if(checkWinner() || turn >= 8){
       $("td").off("click")
+    } else {
+      $("td").on("click", function(event) {
+        // this is only necessary due to how the tests are written.  I think it looks ugly.
+        doTurn(event.target)
+      })
     }
   })
 }
 
 
 function createBoardArray() {
-  var count = 0
-  var board = []
-  for(var y = 0; y < 3; y++ ){
-    for(var x = 0; x < 3; x++){
+  let count = 0
+  // let board = []
+  for(let y = 0; y < 3; y++ ){
+    for(let x = 0; x < 3; x++){
       board[count] = $(`[data-x="${x}"][data-y="${y}"]`).text()
       count ++
     }
@@ -155,19 +156,25 @@ function createBoardArray() {
 }
 
 function createBoard() {
-  var squares = $("td")
-  for(var i = 0; i < squares.length; i++ ){
+  let squares = $("td")
+  for(let i = 0; i < squares.length; i++ ){
     squares[i].innerHTML = board[i]
     }
 }
 
 function setTurn() {
-  var squares = $("td")
-  var count = 0
-  for(var i = 0; i < 9; i++) {
+  let squares = $("td")
+  let count = 0
+  for(let i = 0; i < 9; i++) {
     if(squares[i].innerHTML != "") {
       count++
     }
   }
   turn = count
+}
+
+function newGame () {
+  saveGame()
+  clearGame()
+  board = createBoardArray()
 }
